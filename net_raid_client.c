f351952f7417ed2f376d0c573dc9b01601d93b04
vashli <mberi15@freeuni.edu.ge>
2018-07-28 17:44:40 +0400

    Execv new fuse file

--- net_raid_client.c
+++ net_raid_client.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "config_parser.c"
+#include <sys/wait.h>
+
 int main(int argc, char *argv[]){
     printf("client main\n");
     parse_config_file("config_file");
+
+    int i = 0;
+    for(; i < storages_count; i++){
+        struct storage_data stor = storages_data[i];
+        if(stor.raid == 1){
+            printf("stor.mountpoint %s\n", stor.mountpoint );
+
+            pid_t pid = fork();
+            if(pid == (pid_t) -1) perror("couldn fork");
+            else if (pid == 0) { // child process
+                char * argv[2];
+                argv[1] = "-f";
+                argv[2] = stor.mountpoint;
+
+                int fuse_execv =execv("ssfs/ssfs", argv);
+                if(fuse_execv == -1){
+                    printf("couldnt execv\n");
+                }
+            }
+            else{
+
+                printf("in parent \n" );
+                waitpid(-1, NULL, WUNTRACED);
+            }
+
+
+
+        }
+    }
+
     return 0;
 
 }
